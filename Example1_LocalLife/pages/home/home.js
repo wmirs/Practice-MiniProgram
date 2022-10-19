e65a047e7e3408ade1e8c648175108fdc2b1c792
wangmu <hinanawicirno@outlook.com>
2022-10-19 13:57:36 +0800

    merge

--- Example1_LocalLife/pages/home/home.js
+++ Example1_LocalLife/pages/home/home.js
@@ -5,21 +5,39 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 轮播图
+    swiperList: [
+      // {
+      //   "id": 1,
+      //   "image": "http://ww1.sinaimg.cn/mw690/006ThXL5ly1fj7zx3w751j30u00dmgy3.jpg"
+      // }
+    ]
+  },
 
+  getSwiperList() {
+    wx.request({
+      url: 'https://www.escook.cn/slides',
+      method: 'GET',
+      success: (res) => {
+        this.setData({
+          swiperList: res.data
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getSwiperList()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    console.log("data: ", this.data.swiperList)
   },
 
   /**
